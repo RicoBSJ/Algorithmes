88b496b7c725e3d717db90ce09d456a9b02d0578
RicoBSJ <aubrun.eric@free.fr>
2019-04-23 00:30:11 +0200

    5 Commit Home 23_04

---
+++ src/CourseAvecLesEnfants.java
@@ -0,0 +1,24 @@
+//import static algorea.Robot.*;
+//
+//class CourseAvecLesEnfants {
+//	public static void main(String[] args) {
+//		for (int loop = 1; loop <= 10; loop = loop + 1) {
+//			droite();
+//		}
+//		ramasser();
+//		for (int loop = 0; loop <= 10; loop = loop + 1) {
+//			gauche();
+//		}
+//		deposer();
+//	}
+//}
+
+/*
+ * Pour chaque anneau
+ * 		Aller chercher l'anneau
+ * 		Ramener l'anneau
+ * 
+ * Répéter selon l'anneau
+ * 		Aller à droite
+ * Ramasser l'anneau
+ */
